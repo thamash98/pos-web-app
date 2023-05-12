d2d97dc906d1d22ca22d77e8bc990b716a8f5dd1
thamash98 <thasaranga123@gmail.com>
2023-05-12 15:52:23 +0530

    item entity @ lombok added

---
+++ src/main/java/com/springbootproject/possystem/entity/Item.java
@@ -0,0 +1,40 @@
+package com.springbootproject.possystem.entity;
+
+import com.springbootproject.possystem.entity.enums.MeasuringUnitType;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "item")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Item {
+
+    @Id
+    @Column(name="item_id",length = 255)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int itemId;
+
+    @Column(name="item_name", length = 100, nullable = false)
+    private String itemName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="measure_type", length = 50, nullable = false)
+    private MeasuringUnitType measuringUnitType;
+
+    @Column(name="qty_on_hand", nullable = false)
+    private double qtyOnHand;
+
+    @Column(name="supplier_price", nullable = false)
+    private double supplierPrice;
+
+    @Column(name ="selling_price",nullable = false)
+    private double sellingPrice;
+
+    @Column(name = "active_state", columnDefinition = "TINYINT default 0")
+    private boolean activeState;
+}
---
+++ src/main/java/com/springbootproject/possystem/entity/enums/MeasuringUnitType.java
@@ -0,0 +1,9 @@
+package com.springbootproject.possystem.entity.enums;
+
+public enum MeasuringUnitType {
+    KILO_GRAM,
+    GRAM,
+    LITER,
+    MILLI_LITTER,
+    NUMBER
+}
